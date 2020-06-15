9105269b4eb44775cf2f132360d9eb52f9735bf7
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-06-15 21:34:09 +0200

    finished bubble

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -8,7 +8,6 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
 
-
         # TO-DO: swap
         # Your code here
 
@@ -18,9 +17,22 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    i = 0
+    swaps = False
+    while i < len(arr) - 1:
+        if arr[i] > arr[i + 1]:
+            temp = arr[i]
+            arr[i] = arr[i + 1]
+            arr[i+1] = temp
+            swaps = True
 
-
+        if i == len(arr) - 2 and swaps:
+            i = 0
+            swaps = False
+        else:
+            i += 1
     return arr
+
 
 '''
 STRETCH: implement the Count Sort function below
@@ -39,8 +51,9 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
-
 
     return arr
