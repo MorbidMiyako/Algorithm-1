2360b9bd05d93abbc7ce69cb812e9a4b7f98cd7e
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-06-15 22:33:56 +0200

    finished mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(0, len(arr)):
+        if arr[i] is target:
+            return i
 
-
-    return -1   # not found
+    return -1  # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
-
+    lowest = 0
+    highest = len(arr) - 1
+    middle = len(arr)//2
+    while lowest <= highest:
+        middle = (lowest + highest)//2
+        if arr[middle] is target:
+            return middle
+        if arr[middle] > target:
+            highest = middle - 1
+        if arr[middle] < target:
+            lowest = middle + 1
 
     return -1  # not found
